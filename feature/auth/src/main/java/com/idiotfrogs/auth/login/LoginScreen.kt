923de5c6db458866d83a5e45646e35e23e113017
hyeonseo <s21035@gsm.hs.kr>
2025-02-28 08:40:54 +0900

    :sparkles: :: LocalLoginManager 적용

--- feature/auth/src/main/java/com/idiotfrogs/auth/login/LoginScreen.kt
+++ feature/auth/src/main/java/com/idiotfrogs/auth/login/LoginScreen.kt
@@ -7,33 +7,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.idiotfrogs.data.LoginManager
+import com.idiotfrogs.auth.util.LocalLoginManager
 
 @Composable
 fun LoginScreen(
     loginViewModel: LoginViewModel = viewModel(),
 ) {
     val uiState by loginViewModel.uiState.collectAsStateWithLifecycle()
-    val context = LocalContext.current
-    val loginManager = remember { LoginManager(context) }
-
-    Column(modifier = Modifier.fillMaxSize()) {
-        Button(
-            onClick = { loginViewModel.socialLogin { loginManager.googleLogin() } }
-        ) {
-            Text(text = "google login")
-        }
-        Button(
-            onClick = { loginViewModel.socialLogin { loginManager.appleLogin() } }
-        ) {
-            Text(text = "apple login")
-        }
-    }
+    val loginManager = LocalLoginManager.current
 
     when(val state = uiState) {
         is LoginUiState.Error -> {
@@ -43,6 +27,22 @@ fun LoginScreen(
         }
         LoginUiState.Init -> {
             // 화면 로딩 로직
+            if (loginManager != null) {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Button(
+                        onClick = { loginViewModel.socialLogin { loginManager.googleLogin() } }
+                    ) {
+                        Text(text = "google login")
+                    }
+                    Button(
+                        onClick = { loginViewModel.socialLogin { loginManager.appleLogin() } }
+                    ) {
+                        Text(text = "apple login")
+                    }
+                }
+            } else {
+                // loginManager error handling
+            }
         }
         LoginUiState.Success -> {
             // 화면 로직
--- feature/auth/src/main/java/com/idiotfrogs/auth/util/LocalLoginManager.kt
+++ feature/auth/src/main/java/com/idiotfrogs/auth/util/LocalLoginManager.kt
@@ -3,6 +3,6 @@ package com.idiotfrogs.auth.util
 import androidx.compose.runtime.staticCompositionLocalOf
 import com.idiotfrogs.data.LoginManager
 
-val LocalLoginManager = staticCompositionLocalOf<LoginManager> {
-    error("No AuthManager provided")
+val LocalLoginManager = staticCompositionLocalOf<LoginManager?> {
+    null
 }
