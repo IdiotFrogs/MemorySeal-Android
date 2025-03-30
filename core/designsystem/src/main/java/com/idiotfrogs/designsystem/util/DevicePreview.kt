4e45f1a630cb687b8e9f84bbad71a6534b28cae3
Hyeonseo4799 <s21035@gsm.hs.kr>
2025-03-30 13:13:58 +0900

    :sparkles: :: 로그인 화면 구현

---
+++ core/designsystem/src/main/java/com/idiotfrogs/designsystem/util/DevicePreview.kt
@@ -0,0 +1,11 @@
+package com.idiotfrogs.designsystem.util
+
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "PIXEL_2", device = Devices.PIXEL_2)
+@Preview(name = "PIXEL_3", device = Devices.PIXEL_3)
+@Preview(name = "PIXEL_4", device = Devices.PIXEL_4)
+@Preview(name = "PIXEL_5", device = Devices.PIXEL_5)
+@Preview(name = "PIXEL_6", device = Devices.PIXEL_6)
+annotation class DevicePreview
