75d55f1c38959358759b92c5ee00a178db7aacc5
Hyeonseo4799 <s21035@gsm.hs.kr>
2025-03-30 15:37:08 +0900

    :fire: :: 불필요한 import문 삭제

--- core/designsystem/src/main/java/com/idiotfrogs/designsystem/theme/MSTheme.kt
+++ core/designsystem/src/main/java/com/idiotfrogs/designsystem/theme/MSTheme.kt
@@ -1,6 +1,5 @@
 package com.idiotfrogs.designsystem.theme
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 
