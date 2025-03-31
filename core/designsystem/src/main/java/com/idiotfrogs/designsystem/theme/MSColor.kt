1380e79299710f407ae2e9f3177045b5fa2137ba
hyeonseo <s21035@gsm.hs.kr>
2025-04-01 08:35:59 +0900

    :lipstick: :: 변경된 Color system 적용

--- core/designsystem/src/main/java/com/idiotfrogs/designsystem/theme/MSColor.kt
+++ core/designsystem/src/main/java/com/idiotfrogs/designsystem/theme/MSColor.kt
@@ -9,11 +9,6 @@ private val PrimaryLight = Color(0xFF9EE5B0)
 private val PrimaryNormal = Color(0xFF34C759)
 private val PrimaryDark = Color(0xFF2EAE4D)
 
-// Secondary
-private val SecondaryLight = Color(0xFFFAE876)
-private val SecondaryNormal = Color(0xFFE0C409)
-private val SecondaryDark = Color(0xFFCEB408)
-
 // Grey
 private val GreyG1 = Color(0xFFE6E6E6)
 private val GreyG2 = Color(0xFFD2D3D6)
@@ -21,10 +16,16 @@ private val GreyG3 = Color(0xFF919191)
 private val GreyG4 = Color(0xFF454545)
 private val GreyG5 = Color(0xFF1A1A1A)
 
-// System
-private val Red = Color(0xFFEC5151)
-private val Blue = Color(0xFF6393EE)
+// Background
+private val BGNormal = Color(0xFFF5F5F5)
 
+// System
+private val Red = Color(0xFFDD0303)
+private val Blue = Color(0xFF5C8EED)
+
+// Static
+private val Black = Color(0xFF000000)
+private val White = Color(0xFFFFFFFF)
 
 @Immutable
 data class MSColor(
@@ -33,10 +34,8 @@ data class MSColor(
     val primaryNormal: Color = PrimaryNormal,
     val primaryDark: Color = PrimaryDark,
 
-    // secondary
-    val secondaryLight: Color = SecondaryLight,
-    val secondaryNormal: Color = SecondaryNormal,
-    val secondaryDark: Color = SecondaryDark,
+    // background
+    val bgNormal: Color = BGNormal,
 
     // grey
     val greyG1: Color = GreyG1,
@@ -48,6 +47,10 @@ data class MSColor(
     // system
     val red: Color = Red,
     val blue: Color = Blue,
+
+    // static
+    val black: Color = Black,
+    val white: Color = White,
 )
 
 val LocalMSColor = staticCompositionLocalOf { MSColor() }
