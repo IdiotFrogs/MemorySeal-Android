412d515ac239a25d69c9e850bea1f4032da76767
sunghyun <pjhomg5530@naver.com>
2025-05-30 08:53:47 +0900

    :recycle: Keyboard 로직 함수화

--- core/designsystem/src/main/java/com/idiotfrogs/designsystem/util/KeyboardUtil.kt
+++ core/designsystem/src/main/java/com/idiotfrogs/designsystem/util/KeyboardUtil.kt
@@ -1,11 +1,14 @@
 package com.idiotfrogs.designsystem.util
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.ime
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
@@ -20,4 +23,13 @@ fun rememberKeyboardVisibility(): Boolean {
     }
 
     return keyboardVisible
+}
+
+@Composable
+fun Modifier.keyboardAutoScroll(scrollState: ScrollState): Modifier {
+    val isKeyboardVisible = rememberKeyboardVisibility()
+
+    LaunchedEffect(isKeyboardVisible) { if (isKeyboardVisible) { scrollState.scrollTo(scrollState.maxValue) } }
+
+    return this@keyboardAutoScroll
 }
