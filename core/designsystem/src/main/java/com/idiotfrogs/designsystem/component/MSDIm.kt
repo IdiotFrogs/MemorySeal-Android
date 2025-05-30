c67e59e02948f80b561bb99d7f8dbbd7dca3ac33
hyeonseo <s21035@gsm.hs.kr>
2025-05-30 11:10:02 +0900

    :recycle: :: noRippleClickable 사용

--- core/designsystem/src/main/java/com/idiotfrogs/designsystem/component/MSDIm.kt
+++ core/designsystem/src/main/java/com/idiotfrogs/designsystem/component/MSDIm.kt
@@ -4,8 +4,6 @@ import androidx.compose.animation.core.FiniteAnimationSpec
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -16,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.idiotfrogs.designsystem.util.noRippleClickable
 
 @Composable
 fun MSDim(
@@ -31,15 +30,11 @@ fun MSDim(
     )
 
     if (alpha > 0f) {
-        val interactionSource = remember { MutableInteractionSource() }
-
         Box(
             Modifier
                 .fillMaxSize()
                 .background(color.copy(alpha = color.alpha * alpha))
-                .clickable(
-                    interactionSource = interactionSource,
-                    indication = null,
+                .noRippleClickable(
                     onClick = onDismiss
                 )
         )
