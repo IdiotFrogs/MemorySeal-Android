0a1b641196d294bd6c245acc2348e12bee101d1b
hyeonseo <s21035@gsm.hs.kr>
2025-07-12 14:34:35 +0900

    Revert ":lipstick: :: 버튼 디자인 변경"
    This reverts commit c372c749172226a68a97731d2ff43d180031e1d2.

--- feature/auth/src/main/java/com/idiotfrogs/auth/signup/SignUpScreen.kt
+++ feature/auth/src/main/java/com/idiotfrogs/auth/signup/SignUpScreen.kt
@@ -194,6 +194,7 @@ fun SignUpScreen(
                 .padding(
                     horizontal = if (isShowKeyboard) 0.dp else 20.dp
                 ),
+            isRounded = !isShowKeyboard,
             content = {
                 MSText(
                     text = "이 프로필로 할게요!",
