e5e03e39a58ca88494cd902aae872d6d9a3c4974
sunghyun <pjhomg5530@gmail.com>
2026-04-01 17:11:48 +0900

    :recycle: UiState Success시 data type 생성

--- core/util/src/main/java/com/idiotfrogs/util/UiState.kt
+++ core/util/src/main/java/com/idiotfrogs/util/UiState.kt
@@ -1,7 +1,7 @@
 package com.idiotfrogs.util
 
-sealed interface UiState {
-    data object Init : UiState
-    data object Success : UiState
-    data class Error(val errorMessage: String?) : UiState
+sealed interface UiState<out T> {
+    data object Init : UiState<Nothing>
+    data class Success<T>(val data: T) : UiState<T>
+    data class Error(val errorMessage: String?) : UiState<Nothing>
 }
