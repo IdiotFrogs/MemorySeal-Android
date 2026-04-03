c4d8f08d26412aadf64a9d014b5bea7c359290e4
sunghyun <pjhomg5530@gmail.com>
2026-04-03 15:46:23 +0900

    :arrow_up: orbit 버전 업그레이드
    - 6.0.0 -> 11.0.0

--- feature/setting/src/main/java/com/idiotfrogs/setting/SettingViewModel.kt
+++ feature/setting/src/main/java/com/idiotfrogs/setting/SettingViewModel.kt
@@ -4,9 +4,6 @@ import com.idiotfrogs.util.UiState
 import com.idiotfrogs.util.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.Container
-import org.orbitmvi.orbit.ContainerHost
-import org.orbitmvi.orbit.syntax.simple.intent
-import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
