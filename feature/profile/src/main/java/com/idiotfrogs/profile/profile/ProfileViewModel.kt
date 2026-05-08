0cb327349f5622b256f103e7353be0e7426f0a74
hyeonseo <ragdoll4799@gmail.com>
2026-05-09 01:00:33 +0900

    :sparkles: :: 프로필 API 연동

--- feature/profile/src/main/java/com/idiotfrogs/profile/profile/ProfileViewModel.kt
+++ feature/profile/src/main/java/com/idiotfrogs/profile/profile/ProfileViewModel.kt
@@ -1,34 +1,70 @@
 package com.idiotfrogs.profile.profile
 
+import androidx.compose.runtime.Immutable
+import com.idiotfrogs.domain.usecase.timecapsule.GetMyTimeCapsuleUseCase
+import com.idiotfrogs.domain.usecase.user.GetMyProfileUseCase
+import com.idiotfrogs.model.timecapsule.MyTimeCapsuleResponse
+import com.idiotfrogs.model.timecapsule.TimeCapsuleStatus
+import com.idiotfrogs.model.user.ProfileResponse
 import com.idiotfrogs.util.UiState
 import com.idiotfrogs.util.base.BaseViewModel
 import com.idiotfrogs.util.sideEffect.RefreshEvent
 import com.idiotfrogs.util.sideEffect.RefreshSideEffect
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
+    private val getMyTimeCapsuleUseCase: GetMyTimeCapsuleUseCase,
+    private val getMyProfileUseCase: GetMyProfileUseCase
+) : BaseViewModel<UiState<ProfileData>, ProfileSideEffect, ProfileAction>() {
 
-) : BaseViewModel<UiState<Unit>, ProfileSideEffect, ProfileAction>() {
-
-    override val container: Container<UiState<Unit>, ProfileSideEffect> = container(
+    override val container: Container<UiState<ProfileData>, ProfileSideEffect> = container(
         initialState = UiState.Init,
         onCreate = {
             safeLaunch {
-                // TODO 초기 데이터 로딩
-                intent { reduce { UiState.Success(Unit) } }
-
+                fetchProfile()
                 RefreshSideEffect.events.collect {
                     if (it is RefreshEvent.Profile) {
-                        // TODO fetchProfile()
+                        fetchProfile()
                     }
                 }
             }
         }
     )
+
+    private fun fetchProfile() {
+        safeLaunch {
+            val userDeferred = async { getMyProfileUseCase() }
+            val capsulesDeferred = async { getMyTimeCapsuleUseCase() }
+
+            val userResult = userDeferred.await()
+            val capsulesResult = capsulesDeferred.await()
+
+            val results = listOf(userResult, capsulesResult)
+
+            intent {
+                if (results.any { it.isFailure }) {
+                    reduce { UiState.Error(results.first { it.isFailure }.exceptionOrNull()?.message) }
+                } else {
+                    reduce {
+                        UiState.Success(
+                            ProfileData(
+                                user = userResult.getOrNull(),
+                                capsules = capsulesResult.getOrNull()
+                                    ?.flatMap { it.value }
+                                    ?.filter { it.timeCapsuleStatus == TimeCapsuleStatus.OPENED }
+                                    ?: emptyList(),
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
 
     override fun onAction(action: ProfileAction) {
         when (action) {
@@ -38,6 +74,12 @@ class ProfileViewModel @Inject constructor(
         }
     }
 }
+
+@Immutable
+data class ProfileData(
+    val user: ProfileResponse? = null,
+    val capsules: List<MyTimeCapsuleResponse> = emptyList()
+)
 
 sealed interface ProfileAction {
     data object NavigateToSetting : ProfileAction
